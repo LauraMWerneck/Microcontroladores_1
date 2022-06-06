dd5bbd2dc1c67ae1fd7178b3116421e1a5b6d6f1
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2022-06-06 16:24:07 -0300

    Create main.c

---
+++ Tarefa 03: acionamento PWM/main.c
@@ -0,0 +1,81 @@
+/*
+ * 05_main_simple_pwm.c
+ *
+ *  Created on: Apr 22, 2020
+ *      Author: Renan Augusto Starke
+ *      Instituto Federal de Santa Catarina
+ *
+ *      Exemplo de parcial de aplicação:
+ *
+ *      - Gera multiplos PWMs com o temporizador B3
+ */
+
+#include <msp430.h>
+
+/* Tipos uint16_t, uint8_t, ... */
+#include <stdint.h>
+
+void init_clock_system(void) {             //Inicia o clock da CPU com frequencia de 16MHz
+
+    // Configure two FRAM wait state as required by the device data sheet for MCLK
+    // operation at 16MHz(beyond 8MHz) _before_ configuring the clock system.
+    FRCTL0 = FRCTLPW | NWAITS_2 ;
+
+    P2SEL1 |= BIT6 | BIT7;                       // P2.6~P2.7: crystal pins
+    do
+    {
+        CSCTL7 &= ~(XT1OFFG | DCOFFG);           // Clear XT1 and DCO fault flag
+        SFRIFG1 &= ~OFIFG;
+    } while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
+
+    __bis_SR_register(SCG0);                     // disable FLL
+    CSCTL3 |= SELREF__XT1CLK;                    // Set XT1 as FLL reference source
+    CSCTL0 = 0;                                  // clear DCO and MOD registers
+    CSCTL1 = DCORSEL_5;                          // Set DCO = 8MHz
+    CSCTL2 = FLLD_0 + 489;                       // DCOCLKDIV = 32735*489 / 1
+    __delay_cycles(3);
+    __bic_SR_register(SCG0);                     // enable FLL
+    while(CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1));   // FLL locked
+
+    /* CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK;
+     * set XT1 (~32768Hz) as ACLK source, ACLK = 32768Hz
+     * default DCOCLKDIV as MCLK and SMCLK source
+     - Selects the ACLK source.
+     * 00b = XT1CLK with divider (must be no more than 40 kHz)
+     * 01b = REFO (internal 32-kHz clock source)
+     * 10b = VLO (internal 10-kHz clock source) (1)   */
+    CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK;
+}
+
+
+void main(void)
+{
+    /* Para o watchdog timer
+     * Necessário para código em depuração */
+    WDTCTL = WDTPW | WDTHOLD;
+
+#if defined (__MSP430FR2355__)
+    /* Disable the GPIO power-on default high-impedance mode */
+    PM5CTL0 &= ~LOCKLPM5;
+#endif
+
+    init_clock_system();
+
+    inicializa_motores();
+
+
+    while(1)
+    {
+        void motor_para_frente(2000);
+
+        __delay_cycles(1000);
+
+        void motor_para_tras(2000);
+
+        __delay_cycles(1000);
+
+        __bis_SR_register(LPM0_bits + GIE);
+
+        /* Código de baixa prioridade da aplicação */
+    }
+}
