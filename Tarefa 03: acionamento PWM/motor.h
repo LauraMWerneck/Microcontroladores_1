b86d345c49f32486961dd548abb8167114739e79
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2022-06-06 16:24:31 -0300

    Create motor.h

---
+++ Tarefa 03: acionamento PWM/motor.h
@@ -0,0 +1,19 @@
+/*
+ * motor.h
+ *
+ *  Created on: 06/06/2022
+ *      Author: laura
+ */
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+
+void inicializa_motores();
+
+void motor_para_frente(uint16_t x);
+
+void motor_para_tras(uint16_t x);
+
+
+#endif /* MOTOR_H_ */
