ff85a0bbb0eee35078ab7fd21e5d886734cd2992
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2022-06-10 14:18:05 -0300

    Update main.c

--- Tarefa 02: aplicação LCD 16x2/main.c
+++ Tarefa 02: aplicação LCD 16x2/main.c
@@ -1,82 +1,89 @@
 /*
- * Nome: Laura Martin Werneck
- * Data: 09/05/2022
+ * 05_main_simple_pwm.c
  *
- * Requisitos: Implemente uma aplicação de contagem pulsos para uma industria
- * de alimentos. O sensor de entrada deve ser conectado a um gerador de funções.
- * Um display LCD deve exibir a quantidade pulsos  em tempo real.
+ *  Created on: Apr 22, 2020
+ *      Author: Renan Augusto Starke
+ *      Instituto Federal de Santa Catarina
  *
- * Descricao: Este codigo configura um display LCD para mostrar a contagem de pulsos
- * de um gerador de função, a partir de sua borda de subida. Para isso ele apresenta
- * em seu display as seguintes informacoes:
- *                         ------------------------------
- *                         | Quant. pulsos:             |
- *                         | Número                     |
- *                         ------------------------------
- * Caso o gerador chegue em 100.000 pulsos, o display mostra que chegou no valor máximo,
- * que foi especificado no projeto.
- * Além de configurar o display tambem foi feito um botao de reset para zerar a contagem
- * a qualquer momento pelo usuário.
+ *      Exemplo de parcial de aplicação:
  *
- * Os pinos usados para essa implementacao estao representados abaixo segundo o diagrama
- * de pinos:
- *                             MSP430FR2355
- *                       -------------------------
- *                       |                       |
- *         Botao RESET-->|P4.1                   |
- *                       |                       |
- *                       |                   P6.0|--> D4 do LCD
- *                       |                   P6.1|--> D5 do LCD
- *                       |                   P6.2|--> D6 do LCD
- *                  RS-->|P1.3               P6.3|--> D7 do LCD
- *                   E-->|P1.2                   |
- *                       |                  P2.5 |--> Sensor de entrada conectado a um gerador de funções
- *                       -------------------------
- * */
+ *      - Gera multiplos PWMs com o temporizador B3
+ */
 
-#include <msp430.h> 
-#include <stdio.h>
+#include <msp430.h>
+
+/* Tipos uint16_t, uint8_t, ... */
+#include <stdint.h>
 
 #include "gpio.h"
-#include "lcd.h"
+#include "bits.h"
 
-#define BUTTON  BIT1           // Porta P4.1 - botao usado como reset
-#define SINAL_PORT P2         // Porta P2 como a que recebe o sinal do gerador de funcao
-#define SINAL  BIT5         // Porta P2.5 // ///
+#include "motor.h"
 
-#define MAX_PULSES 100000      // Definicao do numero maximo de pulsos
 
-volatile uint32_t pulses = 0;  // Inicializa a quantidade de pulsos em zero
+#define BUTTON_PORT_1 P4
+#define BUTTON_1 BIT1
+#define BUTTON_PORT_2 P2
+#define BUTTON_2 BIT3
 
-void hardware_init()           // Configuracoes do botao de reset
+
+#define BUTTON_SAMPLES (12)
+
+
+void init_clock_system(void) {             //Inicia o clock da CPU com frequencia de 16MHz
+
+    // Configure two FRAM wait state as required by the device data sheet for MCLK
+    // operation at 16MHz(beyond 8MHz) _before_ configuring the clock system.
+    FRCTL0 = FRCTLPW | NWAITS_2 ;
+
+    P2SEL1 |= BIT6 | BIT7;                       // P2.6~P2.7: crystal pins
+    do
+    {
+        CSCTL7 &= ~(XT1OFFG | DCOFFG);           // Clear XT1 and DCO fault flag
+        SFRIFG1 &= ~OFIFG;
+    } while (SFRIFG1 & OFIFG);                   // Test oscillator fault flag
+
+    __bis_SR_register(SCG0);                     // disable FLL
+    CSCTL3 |= SELREF__XT1CLK;                    // Set XT1 as FLL reference source
+    CSCTL0 = 0;                                  // clear DCO and MOD registers
+    CSCTL1 = DCORSEL_5;                          // Set DCO = 8MHz
+    CSCTL2 = FLLD_0 + 489;                       // DCOCLKDIV = 32735*489 / 1
+    __delay_cycles(3);
+    __bic_SR_register(SCG0);                     // enable FLL
+    while(CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1));   // FLL locked
+
+    /* CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK;
+     * set XT1 (~32768Hz) as ACLK source, ACLK = 32768Hz
+     * default DCOCLKDIV as MCLK and SMCLK source
+     - Selects the ACLK source.
+     * 00b = XT1CLK with divider (must be no more than 40 kHz)
+     * 01b = REFO (internal 32-kHz clock source)
+     * 10b = VLO (internal 10-kHz clock source) (1)   */
+    CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK;
+}
+
+
+void config_timerB_0(){
+    /* Timer B0:
+     * TASSEL_2 -> Clock de SMCLK.
+     * MC_1 -> Contagem crescente até CCR0.
+     * ID_2 -> Prescaler = /4
+     */
+    TB0CTL = TBSSEL_2 | MC_1 | ID_2;
+
+    /* IRQ por comparação entre contagem e comparador 0 */
+    TB0CCTL0 = CCIE;
+    /* Valor de comparação é 40000 */
+    TB0CCR0 = 40000;
+}
+
+
+void main(void)
 {
-    /* Habilita resistor de pull up ou pull down */
-    P4REN |= BUTTON;
-    /* Habilita resitor como pull up */
-    P4OUT |= BUTTON;
-}
+    volatile uint16_t my_data = 0;
 
-void config_ext_irq(){            // Configuracoes da interrupcao
-
-    /* Habilitacao da IRQ apenas botao */
-    PORT_IE(SINAL_PORT) =  SINAL;
-
-    /* Transicao de nivel baixo para alto - borda de subida */
-    /*Como queremos tudo em 0 nao precisa do comando abaixo
-     * Port x interrupt edge select
-     * 0b = PxIFG flag is set with a low-to-high transition*/
-    //PORT_IES(BUTTON_PORT) = BUTTON_0;
-
-    /* Limpa alguma IRQ pendente */
-    PORT_IFG(SINAL_PORT) &= ~SINAL;
-}
-
-
-void main(){
-
-    char string[16];
-
-    /* Configuracao de hardware */
+    /* Para o watchdog timer
+     * Necessário para código em depuração */
     WDTCTL = WDTPW | WDTHOLD;
 
 #if defined (__MSP430FR2355__)
@@ -84,65 +91,71 @@ void main(){
     PM5CTL0 &= ~LOCKLPM5;
 #endif
 
-    /* Incializa o hardware */
-    hardware_init();
 
-    /* Inicializa hardare: veja lcd.h para
-     * configurar pinos */
-    lcd_init_4bits();
-    /* Escreve string */
+    /* Configura botões */
+    /* BUTTON_PORT totalmente como entrada */
+    PORT_DIR(BUTTON_PORT_1) = 0;
+    PORT_DIR(BUTTON_PORT_2) = 0;
+    /* Resistores de pull up */
+    PORT_REN(BUTTON_PORT_1) = BUTTON_1;
+    PORT_OUT(BUTTON_PORT_1) = BUTTON_1;
+    PORT_REN(BUTTON_PORT_2) = BUTTON_2;
+    PORT_OUT(BUTTON_PORT_2) = BUTTON_2;
 
-    /* Configura interup��es */
-    config_ext_irq();
+    /* Configurações de hardware */
+    init_clock_system();
+    config_timerB_0();
+    inicializa_motores();
 
-    /* Habilita IRQs e desliga CPU */
-    __bis_SR_register(GIE);
 
-    lcd_send_data(LCD_LINE_0, LCD_CMD);    // Imprime na primeira linha "Quant. pulsos:"
-    lcd_write_string("Quant. pulsos:");
+    __bis_SR_register(LPM0_bits + GIE);
 
-    _delay_cycles(100000);
+    while(1)
+    {
+        //__bis_SR_register(LPM0_bits + GIE);
 
-    while (1){
-
-        lcd_send_data(LCD_LINE_1, LCD_CMD);  // Imprime na segunda linha o valor da quantidade de pulsos
-          snprintf(string, 16, "%d", pulses);
-        lcd_write_string(string);
-
-        /* Botao 1: reset. Quando pressionado, o valor do contador e zerado.
-         * Deteccao na funcao main, sem interrupcao externa.
-         * Quando o botao for pressionado manda 0, so que isso nao daria verdadeiro entao por isso o '!'*/
-        if (!TST_BIT(P4IN, BUTTON)){ /* Equivalente a: if (P4IN & BIT1) */
-            pulses = 0;
-            lcd_send_data(LCD_LINE_1, LCD_CMD);
-            lcd_write_string("                 ");
-        }
-
-        if(pulses >= MAX_PULSES){                  // Se a quant. máxima de pulsos for atingido imprime a mensagem
-            lcd_send_data(LCD_LINE_1, LCD_CMD);
-            lcd_write_string("Maximo atingido!");
-        }
-
-        _delay_cycles(10000);
+        /* Código de baixa prioridade da aplicação */
     }
-
 }
 
-/* Port 1 ISR (interrupt service routine) */
+
+/* ISR0 do Timer B: executado no evento de comparação  comparador 0 (TBCCR0)
+ *
+ * Utilizado para o debouncer por amostragem: faz a verificação de botão
+ * periodicamente.
+ *
+ * */
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=PORT2_VECTOR
-__interrupt void Port_2(void)
+#pragma vector=TIMER0_B0_VECTOR
+__interrupt void TIMER0_B0_ISR(void)
 #elif defined(__GNUC__)
-void __attribute__ ((interrupt(PORT1_VECTOR))) Port_1 (void)
+void __attribute__ ((interrupt(TIMER0_B0_VECTOR))) Timer_B (void)
 #else
 #error Compiler not supported!
 #endif
 {
+    static uint8_t counter_b1 = BUTTON_SAMPLES;
+    static uint8_t counter_b2 = BUTTON_SAMPLES;
 
-    if(pulses < MAX_PULSES)  //Enquanto pulsos for enos que o numero maximo incrementa pulsos
-        pulses++;
+    /* Se botão 1 apertado: borda de descida */
+    if (!TST_BIT(PORT_IN(BUTTON_PORT_1), BUTTON_1))  {
+        /* Se contagem = 0, debounce terminado */
+        if (!(--counter_b1)) {
 
-    /* Limpa sinal de IRQ do bot�o 0 */
-    PORT_IFG(SINAL_PORT) &= ~SINAL;
+            muda_razao_ciclica();
+        }
+    }
+    else
+        counter_b1 = BUTTON_SAMPLES;
+
+    if (!TST_BIT(PORT_IN(BUTTON_PORT_2), BUTTON_2))  {
+        /* Se contagem = 0, debounce terminado */
+        if (!(--counter_b2)) {
+
+            muda_sentido();
+
+        }
+    }
+    else
+        counter_b2 = BUTTON_SAMPLES;
 }
-
