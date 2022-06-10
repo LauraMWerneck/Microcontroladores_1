c59d544bb3afced57d0f678dcde4ddbbc70eae5b
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2022-06-10 14:22:37 -0300

    Update motor.c

--- Tarefa 03: acionamento PWM/motor.c
+++ Tarefa 03: acionamento PWM/motor.c
@@ -10,10 +10,12 @@
 
 #include "motor.h"
 
+#define CONTAGEM_MAX_CCR 8000
+
+
 void config_timerB_3_as_pwm();
 
-
-enum{DESLIGADO, FRENTE, TRAS, ESQUERDA, DIREITA};
+enum {DESLIGADO, FRENTE, TRAS, ESQUERDA, DIREITA};
 
 struct estado_motores{
     uint8_t direcao;
@@ -24,7 +26,7 @@ volatile struct estado_motores estado_carrinho = {DESLIGADO, 0};
 
 
 /*
- * @brief  Configura temporizador B3 com contagem up e down.
+ * Configura temporizador B3 com contagem up e down.
  */
 void config_timerB_3_as_pwm(){
 
@@ -33,7 +35,7 @@ void config_timerB_3_as_pwm(){
      * é possível usar o overflow */
 
     /* Configuração dos comparadores como PWM:
-     *S
+     *
      * TB3CCR0: Timer3_B Capture/Compare 0: período do PWM
      *
      * OUTMOD_2: PWM output mode: 2 - PWM toggle/reset
@@ -41,28 +43,28 @@ void config_timerB_3_as_pwm(){
      * TB3CCR1 PWM duty cycle: TB3CCR1 / TB3CCR0 *
      * TA2CCR1 PWM duty cycle: TA2CCR1 / TA1CCR0 */
 
-    TB3CCR0 = 8000-1;
+    TB3CCR0 = CONTAGEM_MAX_CCR-1;
 
 
     /*      .
-    *      /|\                  +                < -Comparador 0: (máximo da contagem) -> período do PWM
-    *       |                 +   +
-    *       |               +       +
-    *       |-------------+---------- +          <--  Comparadores 1 e 2: razão cíclica
-    *       |           +  |         | +
-    *       |         +    |         |   +
-    *       |       +      |         |     +
-    *       |     +        |         |       +
-    *       |   +          |         |         +
-    *       | +            |         |           +
-    * Timer +--------------|---- ----|-------------->
-    *       |              |
-    *       |
-    *
-    *       |--------------+         |--------------
-    * Saída |              |         |
-    *       +---------------++++++++++------------->
-    */
+     *      /|\                  +                < -Comparador 0: (máximo da contagem) -> período do PWM
+     *       |                 +   +
+     *       |               +       +
+     *       |-------------+---------- +          <--  Comparadores 1 e 2: razão cíclica
+     *       |           +  |         | +
+     *       |         +    |         |   +
+     *       |       +      |         |     +
+     *       |     +        |         |       +
+     *       |   +          |         |         +
+     *       | +            |         |           +
+     * Timer +--------------|---- ----|-------------->
+     *       |              |
+     *       |
+     *
+     *       |--------------+         |--------------
+     * Saída |              |         |
+     *       +---------------++++++++++------------->
+     */
 
     /* TBSSEL_2 -> Timer B clock source select: 2 - SMCLK
      * MC_1     -> Timer B mode control: 1 - Up to CCR0
@@ -168,33 +170,60 @@ void motor_para_esquerda(uint16_t x){
 /* Muda a razao ciclica para + 10% do valor máximo (8000)*/
 void muda_razao_ciclica(){
 
-    estado_carrinho.velocidade = estado_carrinho.velocidade + 800;
+    estado_carrinho.velocidade = estado_carrinho.velocidade - 800;
+
+    if (estado_carrinho.velocidade > CONTAGEM_MAX_CCR){
+        estado_carrinho.velocidade = 6000;
+    }
 
     switch(estado_carrinho.direcao){
     case FRENTE:
         motor_para_frente(estado_carrinho.velocidade);
         break;
     case TRAS:
-        motor_para_frente(estado_carrinho.velocidade);
+        motor_para_tras(estado_carrinho.velocidade);
         break;
     case DIREITA:
-        motor_para_frente(estado_carrinho.velocidade);
+        motor_para_direita(estado_carrinho.velocidade);
         break;
     case ESQUERDA:
-        motor_para_frente(estado_carrinho.velocidade);
+        motor_para_esquerda(estado_carrinho.velocidade);
         break;
     default:
         break;
     }
-
 }
 
 
 void muda_sentido(){
-    if ....
-    
-    estado_carrinho.direcao++;
+
+    switch (estado_carrinho.direcao) {
+    case DESLIGADO:
+        motor_para_frente(4000);
+        break;
+    case FRENTE:
+        motor_para_tras(4000);
+        break;
+    case TRAS:
+        motor_para_direita(4000);
+        break;
+    case DIREITA:
+        motor_para_esquerda(4000);
+        break;
+    case ESQUERDA:
+        estado_carrinho.direcao = DESLIGADO;
+
+        TB3CCTL1 = OUTMOD_0;
+        TB3CCTL2 = OUTMOD_0;
+        TB3CCTL3 = OUTMOD_0;
+        TB3CCTL4 = OUTMOD_0;
+
+        break;
+
+    default:
+        break;
+    }
+
 
 }
-
 
