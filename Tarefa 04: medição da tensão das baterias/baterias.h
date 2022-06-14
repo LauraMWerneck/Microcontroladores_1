186ae677be3d93a432f5f0686d7291c800ce1cef
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2022-06-14 09:12:19 -0300

    Update baterias.h

--- Tarefa 04: medição da tensão das baterias/baterias.h
+++ Tarefa 04: medição da tensão das baterias/baterias.h
@@ -1,1 +1,19 @@
+/*
+ * baterias.h
+ *
+ *  Created on: 14 de jun de 2022
+ *      Author: Aluno
+ */
+
+#ifndef BATERIAS_H_
+#define BATERIAS_H_
+
+void timerB_init();
+
+void init_adc();
+
+uint32_t medicao_baterias();
+
+
+#endif /* BATERIAS_H_ */
 
