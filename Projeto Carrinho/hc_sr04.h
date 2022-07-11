1afc43788f5cf2ed8d5621288195da3f72b652b7
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2022-07-11 16:57:53 -0300

    Create hc_sr04.h

---
+++ Projeto Carrinho/hc_sr04.h
@@ -0,0 +1,25 @@
+/*
+ *  Modulo: distancia.h
+ *
+ *  Nome: Laura Martin Werneck
+ *
+ *  Data: 04/07/2022
+ *
+ *  Descrição: Modulo com as funcoes criadas em  distancia.c
+ */
+
+#ifndef DISTANCIA_H_
+#define DISTANCIA_H_
+
+#include <msp430.h>
+#include <stdint.h>
+#include <bits.h>
+
+void init_clock_system(void);
+void config_timerB_1();
+void config_wd_as_timer();
+void trigger();
+void init_sensor();
+uint32_t medicao_distancia();
+
+#endif /* DISTANCIA_H_ */
